465d398d5b96e8fa9edecec50857857493ffc1a5
Olaoye Oluwatoyin <mindprepopps@gmail.com>
2022-08-05 23:42:52 +0100

    Refactor string argument for a more compact code

--- substring.rb
+++ substring.rb
@@ -1,10 +1,9 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 def substrings(string, dictionary)
-  word = string.downcase
   result = Hash.new(0)
   dictionary.each do |str|
     count = 0
-    word.scan(/#{str}/) do |marked|
+    string.downcase.scan(/#{str}/) do |marked|
       count += 1
       result[marked] = count
     end
